fb7bea61aa1f458f7dbc5867599a41337e9ce134
nicomayer96 <63597265+nicomayer96@users.noreply.github.com>
2021-11-09 11:26:03 -0300

    gastoController corregido

--- src/main/java/com/maktub/controller/GastoController.java
+++ src/main/java/com/maktub/controller/GastoController.java
@@ -6,30 +6,34 @@
 package com.maktub.controller;
 
 import com.maktub.dao.GastoDao;
-import com.maktub.dao.VentaDao;
 import com.maktub.model.Gasto;
-import com.maktub.model.Venta;
-import com.maktub.view.VentaView;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author Nico
  */
+@RestController
+@RequestMapping
+@CrossOrigin(origins= "*")
+
 public class GastoController {
     
-     @GetMapping("/gastos")
-    public ResponseEntity <List<Gasto>> readAll(@RequestParam() int mes, int year) throws Exception{
+     @GetMapping("/verGastos")
+    public ResponseEntity <List<Gasto>> readAll(@RequestParam() int mes) throws Exception{
         try{    
-            List<Gasto> gastos = new ArrayList();  
-            gastos = GastoDao.verGastos(mes, year);
+            List<Gasto> gastos = new ArrayList();
+            gastos = GastoDao.verGastos(mes);
 
             return new ResponseEntity(gastos , HttpStatus.OK);
         }catch(Exception e){
