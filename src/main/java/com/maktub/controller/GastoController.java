e07fdbfe04757bbf80d99efa57c43b5a774ebbf2
nicomayer96 <63597265+nicomayer96@users.noreply.github.com>
2021-11-11 12:00:52 -0300

    Add files via upload

--- src/main/java/com/maktub/controller/GastoController.java
+++ src/main/java/com/maktub/controller/GastoController.java
@@ -51,4 +51,15 @@ public class GastoController {
         
     }
     }
+    @GetMapping("/verMontoGastos")
+    public ResponseEntity verMontoGastos(@RequestParam() int mes) throws Exception{
+        try{    
+            int montoGastos = 0;
+            montoGastos = GastoDao.verMontoGastos(mes);
+
+            return new ResponseEntity(montoGastos , HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity(e , HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
